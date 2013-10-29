66e15fd3e20a489d30e0673c752fdb4236865f50
Vee Satayamas <vsatayamas@gmail.com>
2013-10-29 09:07:26 +0700

    get method

--- bangbut_http.js
+++ bangbut_http.js
@@ -1,23 +1,49 @@
 function createHttp() {
   var xhr = new XMLHttpRequest();
+  
+  function _sendPostRequest(uri, params, fn) {
+    xhr.open("POST", uri, true);
+    var formData = new FormData();
+    for(var k in params) {
+      formData.append(k, params[k]);
+    }
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState == 4) {
+        if(xhr.status == 200) {
+          fn(null, xhr.responseText)
+        } else {
+          fn(new Error("server return error"), xhr.status);
+        }
+      }
+    }
+    xhr.send(formData);    
+  }
+  
+  function _sendGetRequest(uri, params, fn) {
+    var queries = [];
+    for(var k in params) {
+      queries.push(escape(k) + "=" + escape(params[k]));
+    }
+    xhr.open("GET", uri + "?" + queries.join("&"), true);
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState == 4) {
+        if(xhr.status == 200) {
+          fn(null, xhr.responseText)
+        } else {
+          fn(new Error("server return error"), xhr.status);
+        }
+      }
+    }
+    xhr.send();    
+  }
+  
   return {
     "retryList": [],
     "post": function(uri, params, fn) {
-      xhr.open("POST", uri, true);
-      var formData = new FormData();
-      for(var k in params) {
-        formData.append(k, params[k]);
-      }
-      xhr.onreadystatechange = function() {
-        if (xhr.readyState == 4) {
-          if(xhr.status == 200) {
-            fn(null, xhr.responseText)
-          } else {
-            fn(new Error("server return error"), xhr.status);
-          }
-        }
-      }
-      xhr.send(formData);
+      _sendPostRequest(uri, params, fn);
     },
+    "get": function(uri, params, fn) {
+      _sendGetRequest(uri, params, fn);
+    }    
   }
 }
--- test/tests.js
+++ test/tests.js
@@ -3,17 +3,30 @@ describe('BangbutHttp', function(){
   var http;
   var requests;
   var xhr;
+  var realFormData;
   before(function () {
-      xhr = sinon.useFakeXMLHttpRequest();
-      requests = [];
-      xhr.onCreate = function (req) { 
-        requests.push(req); 
-      };
-      http = createHttp();
+    xhr = sinon.useFakeXMLHttpRequest();
+    requests = [];
+    xhr.onCreate = function (req) { 
+      requests.push(req); 
+    };
+    http = createHttp();
+    realFormData = global.FormData;
+    global.FormData = function FormData_() {
+      return {
+        "data": [],
+        "append": function(key, value) {
+          var obj = {};
+          obj[key] = value;
+          this.data.push(obj);
+        }
+      }
+    }
   });
 
   after(function () {
-      xhr.restore();
+    global.FormData = realFormData;
+    xhr.restore();
   });
   
   describe('post', function() {
@@ -24,7 +37,22 @@ describe('BangbutHttp', function(){
         done(null);
       });
       chai.expect(requests[0].method).to.equal("POST")
+      chai.expect(requests[0].requestBody.data[0].x).to.equal(10);
       requests[0].respond(200, { "Content-Type": "plain/text" }, "abc");
     })
   })
+  
+  describe('get', function() {
+    it('should send {y:20} and {m:"x"} and get abcd back via GET method', function(done) {
+      http.get("/titi", {y: 20, "m": "x"}, function(err, data) {
+        chai.expect(err).to.be.null;
+        chai.expect(data).to.equal("abc");
+        done(null);
+      });
+      chai.expect(requests[0].method).to.equal("GET");
+      chai.expect(requests[0].url).to.equal("/titi?y=20&m=x");
+      requests[0].respond(200, { "Content-Type": "plain/text" }, "abc");
+    })
+  })
+  
 })
